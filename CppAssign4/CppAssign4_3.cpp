eae26e53a4bb7bc7726c93d9d99231e059a091ee
nilesh23 <nkatkar23@gmail.com>
2023-09-27 15:21:42 +0530

    CPP Assignment 4_3 updated

--- CppAssign4/CppAssign4_3.cpp
+++ CppAssign4/CppAssign4_3.cpp
@@ -5,139 +5,132 @@ parameter-less constructor. Write accept() and print() functions.
 Also provide add(), subtract(), multiply() and transpose() function.
 */
 
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Matrix
 {
-    private:
-        int **mat;
-        int row;
-        int col;
+private:
+    int **mat;
+    int row;
+    int col;
 
-    public:
-        Matrix()
-        {
-            
-        }
-        Matrix(int row,int col) : row(row),col(col)
-        {
-            this->row = row;
-            this->col = col;
-            mat = new int *[row];   //1D allocation
-            for(int i=0;i<row;i++)
-                mat[i] = new int [col]; //2D allocation
-        }
-        void accept()
-        {
-            for(int i=0;i<row;i++)
-                for(int j=0;j<col;j++)
-                {
-                    cout<<"Enter Matrix : \n";
-                    cout<<"mat[" << i <<"]"
-                        <<"[" <<j <<"] : \t";
-                    cin>>mat[i][j];
-                }
-            cout<<endl;
-            cout<<"*****************************"<<endl;
-        } 
-        void print()
-        {
-            cout<<"Matrix is :"<<endl;
-            for(int i=0;i<row;i++)
+public:
+    Matrix()
+    {
+    }
+    Matrix(int row, int col) : row(row), col(col)
+    {
+        this->row = row;
+        this->col = col;
+        mat = new int *[row]; // 1D allocation
+        for (int i = 0; i < row; i++)
+            mat[i] = new int[col]; // 2D allocation
+    }
+    void accept()
+    {
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
             {
-                for(int j=0;j<col;j++)
-                {
-                    cout<<"mat[" << i <<"]"
-                        <<"[" <<j <<"] : ";
-                    cout<<mat[i][j]<<"\t";
-                }
-                cout<<endl;
+                cout << "Enter Matrix : \n";
+                cout << "mat[" << i << "]"
+                     << "[" << j << "] : \t";
+                cin >> mat[i][j];
             }
-            cout<<"*****************************"<<endl;
-        }
-        void add(Matrix m)
+        cout << endl;
+        cout << "*****************************" << endl;
+    }
+    void print()
+    {
+        cout << "Matrix is :" << endl;
+        for (int i = 0; i < row; i++)
         {
-            Matrix tot(row,col);
-            for(int i=0;i<row;i++)
-                for(int j=0;j<col;j++)
+            for (int j = 0; j < col; j++)
+            {
+                cout << "mat[" << i << "]"
+                     << "[" << j << "] : ";
+                cout << mat[i][j] << "\t";
+            }
+            cout << endl;
+        }
+        cout << "*****************************" << endl;
+    }
+    void add(Matrix m)
+    {
+        Matrix tot(row, col);
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+            {
+                tot.mat[i][j] = this->mat[i][j] + m.mat[i][j];
+            }
+        cout << "Addition matrix : \n";
+        tot.print();
+    }
+    void subtract(Matrix m)
+    {
+        Matrix tot(row, col);
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+            {
+                tot.mat[i][j] = this->mat[i][j] - m.mat[i][j];
+            }
+        cout << "Subtraction matrix : \n";
+        tot.print();
+    }
+    void multiply(Matrix m)
+    {
+        Matrix tot(row, col);
+        if (row == m.col)
+        {
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < m.col; j++)
                 {
-                    tot.mat[i][j] = this->mat[i][j] + m.mat[i][j];
+                    tot.mat[i][j] = 0;
+                    for (int k = 0; k < m.col; k++)
+                        tot.mat[i][j] += mat[i][j] * m.mat[k][j];
                 }
-            cout<<"Addition matrix : \n";
+            cout << "Multiplication matrix : \n";
             tot.print();
-        } 
-        void subtract(Matrix m)
+        }
+        else
         {
-            Matrix tot(row,col);
-            for(int i=0;i<row;i++)
-                for(int j=0;j<col;j++)
-                {
-                    tot.mat[i][j] = this->mat[i][j] - m.mat[i][j];
-                }
-            cout<<"Subtraction matrix : \n";
-            tot.print();
-        } 
-        void multiply(Matrix m)
-        {
-            Matrix tot(row,col);
-            if(row==m.col)
-            {
-                for(int i=0;i<row;i++)
-                    for(int j=0;j<m.col;j++)
-                    {
-                        tot.mat[i][j] = 0;
-                        for(int k=0;k<m.col;k++)
-                            tot.mat[i][j] += mat[i][j] * m.mat[k][j];
-                    }
-                cout<<"Multiplication matrix : \n";
-                tot.print();
-            }
-            else
-            {
-                cout<<"Multiplication is not possible."<<endl;
-                cout<<"*****************************"<<endl;
-            }
-        } 
-        void transpose()
-        {
+            cout << "Multiplication is not possible." << endl;
+            cout << "*****************************" << endl;
+        }
+    }
+    void transpose()
+    {
 
-            Matrix tot(row,col);
-            if(row==col)
+        Matrix res(col, row);
+        for (int i = 0; i < col; i++)
+            for (int j = 0; j < row; j++)
             {
-                for(int i=0;i<row;i++)
-                    for(int j=0;j<col;j++)
-                    {
-                        tot.mat[i][j] = this->mat[j][i];
-                    }
-                cout<<"Transpose matrix of second matrix is: \n";
-                tot.print();            
+                res.mat[i][j] = this->mat[j][i];
             }
-            else
-                cout<<"Transpose is not possible"<<endl;
-        }
+        cout << "Transpose matrix of second matrix is: \n";
+        res.print();
+    }
 };
 
 int main()
 {
-    int row,col;
-    cout<<"Enter Matrix dimensions rows and columns : \n";
-    cin>>row>>col;
-    Matrix m1(row,col);
+    int row, col;
+    cout << "Enter Matrix dimensions rows and columns : \n";
+    cin >> row >> col;
+    Matrix m1(row, col);
     m1.accept();
-    m1.print();
-    
+
     cout<<"Enter Matrix dimensions rows and columns : \n";
     cin>>row>>col;
-    Matrix m2(row,col);
+    Matrix m2(row, col);
     m2.accept();
+    m1.print();
     m2.print();
     m1.add(m2);
     m1.subtract(m2);
     m1.multiply(m2);
-    m2.transpose();
+    m1.transpose();
     return 0;
-    cout<<"Thank you..."<<endl;
-    cout<<"*****************************"<<endl;
+    cout << "Thank you..." << endl;
+    cout << "*****************************" << endl;
 }
